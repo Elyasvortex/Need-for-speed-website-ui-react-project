d480065cbb55894a87f2bf0dfc76d42170ef2876
Elyas <eliusp8@gmail.com>
2023-11-28 20:46:05 +0300

    final commit (some files omitted and supersorted)

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,4 @@
-import images from './images';
 import race from '../assets/race.mp4';
-import data from './data';
 
-export { images, race, data };
+
+export { race};
--- src/container/Menu/SpecialMenu.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './SpecialMenu.css';
-
-const SpecialMenu = () => (
- <h1>canceled</h1>
-);
-
-export default SpecialMenu;
